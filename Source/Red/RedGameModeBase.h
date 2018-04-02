efc41e2574d7e985f999e055dfaaf3a3f6cd7089
xxzivadxx <davidllanso@hotmail.com>
2018-04-02 22:01:24 +0200

    Initial commit

---
+++ Source/Red/RedGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RED_API ARedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
