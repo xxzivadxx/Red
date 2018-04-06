19e6fe91d773c4449badce9b220146cdd8b73d98
xxzivadxx <davidllanso@hotmail.com>
2018-04-06 22:06:02 +0200

    enet

--- Source/Red/Red.Build.cs
+++ Source/Red/Red.Build.cs
@@ -4,6 +4,11 @@ using UnrealBuildTool;
 
 public class Red : ModuleRules
 {
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+
 	public Red(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -19,5 +24,37 @@ public class Red : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        LoadBobsMagic(Target);
 	}
+    public bool LoadBobsMagic(ReadOnlyTargetRules Target)
+    {
+        bool isLibrarySupported = false;
+
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "enet", "lib");
+
+            /*
+            test your path with:
+            using System; // Console.WriteLine("");
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            */
+
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "enet.lib"));
+        }
+
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "enet", "includes"));
+        }
+
+        Definitions.Add(string.Format("WITH_ENET_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
 }
